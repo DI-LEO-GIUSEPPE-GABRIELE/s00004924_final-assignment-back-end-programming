59242281d0e59ee0b9d8a5b2747a2472e207c86f
gabrieledileo <gabriele.dileo@hinto.com>
2025-11-19 18:13:48 +0100

    docs(repositories): add method comments to UserRepository

--- src/main/java/study_project/demo/repositories/UserRepository.java
+++ src/main/java/study_project/demo/repositories/UserRepository.java
@@ -8,8 +8,17 @@ import study_project.demo.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Metodo per trovare un utente per email ignorando la case sensitivity
     Optional<User> findByEmailIgnoreCase(String email);
+
+    // Metodo per trovare utenti per nome ignorando la case sensitivity
     List<User> findByNameContainingIgnoreCase(String name);
+
+    // Metodo per trovare utenti per email che termina con un suffisso ignorando la
+    // case sensitivity
     List<User> findByEmailEndingWithIgnoreCase(String suffix);
+
+    // Metodo per trovare utenti per nome e email che terminano con uno specifico
+    // suffisso ignorando la case sensitivity
     List<User> findByNameContainingIgnoreCaseAndEmailEndingWithIgnoreCase(String name, String suffix);
 }
