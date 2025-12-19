59c44eadc09a2c5f945d8462b78d3d4c4965414b
gabrieledileo <gabriele.dileo@hinto.com>
2025-12-19 11:19:00 +0100

    docs: clean up comments and simplify pom.xml descriptions
    Remove Italian comments and simplify dependency descriptions in pom.xml
    Add main class comment in Application.java

--- src/main/java/bluesky/airline/Application.java
+++ src/main/java/bluesky/airline/Application.java
@@ -3,6 +3,7 @@ package bluesky.airline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main class for the application
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
