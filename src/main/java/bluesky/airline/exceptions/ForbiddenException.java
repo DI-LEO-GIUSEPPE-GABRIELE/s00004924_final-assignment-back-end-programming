1af770f93e4daa69cbdf3bbf4538614a79e514e5
gabrieledileo <gabriele.dileo@hinto.com>
2025-12-21 16:34:25 +0100

    refactor(repositories): remove unused imports and delete obsolete exception class
    Remove Page and Pageable imports from CompartmentRepository as they're unused
    Delete ForbiddenException class as it's no longer needed

--- src/main/java/bluesky/airline/exceptions/ForbiddenException.java
+++
@@ -1,8 +0,0 @@
-package bluesky.airline.exceptions;
-
-// Exception for forbidden access
-public class ForbiddenException extends RuntimeException {
-	public ForbiddenException(String message) {
-		super(message);
-	}
-}
--- src/main/java/bluesky/airline/repositories/CompartmentRepository.java
+++ src/main/java/bluesky/airline/repositories/CompartmentRepository.java
@@ -3,8 +3,6 @@ package bluesky.airline.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import bluesky.airline.entities.Compartment;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface CompartmentRepository extends JpaRepository<Compartment, UUID> {
     java.util.Optional<Compartment> findByCompartmentCode(String compartmentCode);
